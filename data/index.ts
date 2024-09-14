a4c578d0b80e18f6069cd1ff31421d1a3661c21c
Divyansh jain <105345627+httpdivyansh@users.noreply.github.com>
2024-09-14 13:00:06 +0530

    image not working update

--- data/index.ts
+++ data/index.ts
@@ -13,7 +13,7 @@ export const gridItems = [
     className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
     imgClassName: "w-full h-full",
     titleClassName: "justify-end",
-    img: "./b1.svg",
+    img: "b1.svg",
     spareImg: "",
   },
   {
@@ -43,8 +43,8 @@ export const gridItems = [
     className: "lg:col-span-2 md:col-span-3 md:row-span-1",
     imgClassName: "",
     titleClassName: "justify-start",
-    img: "./grid.svg",
-    spareImg: "./b4.svg",
+    img: "grid.svg",
+    spareImg: "b4.svg",
   },
 
   {
@@ -54,8 +54,8 @@ export const gridItems = [
     className: "md:col-span-3 md:row-span-2",
     imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
     titleClassName: "justify-center md:justify-start lg:justify-center",
-    img: "./b5.svg",
-    spareImg: "./grid.svg",
+    img: "b5.svg",
+    spareImg: "grid.svg",
   },
   {
     id: 6,
@@ -75,32 +75,32 @@ export const projects = [
     title: "The Weather Channel to watch the weather Forcasting ",
     des: "Explore the wonders of our Weather Application with this captivating 3D simulation of the planets using Three.js.",
     img: "/p1.svg",
-    iconLists: ["./re.svg", "./tail.svg", "./ts.svg", "./three.svg", "./fm.svg"],
-    link: "./ui.earth.com",
+    iconLists: ["re.svg", "tail.svg", "ts.svg", "three.svg", "fm.svg"],
+    link: "ui.earth.com",
   },
   {
     id: 2,
     title: "Text-Summarization Application",
     des: "Simplify your Text with Text Summarization Application.",
-    img: "/p2.svg",
-    iconLists: ["./next.svg", "./tail.svg", "./ts.svg", "./stream.svg", "./c.svg"],
-    link: "./ui.yoom.com",
+    img: "p2.svg",
+    iconLists: ["next.svg", "tail.svg", "ts.svg", "stream.svg", "c.svg"],
+    link: "ui.yoom.com",
   },
   {
     id: 3,
     title: "AI Image SaaS - Canva Application",
     des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
-    img: "/p3.svg",
-    iconLists: ["./re.svg", "./tail.svg", "./ts.svg", "./three.svg", "./c.svg"],
-    link: "./ui.aiimg.com",
+    img: "p3.svg",
+    iconLists: ["re.svg", "tail.svg", "ts.svg", "three.svg", "c.svg"],
+    link: "ui.aiimg.com",
   },
   {
     id: 4,
     title: "Animated Apple store 3D Website",
     des: "Recreated the Apple store website, Using Figma which contain different flavours .",
-    img: "./p4.svg",
-    iconLists: ["./next.svg", "./tail.svg", "./ts.svg", "./three.svg", "./gsap.svg"],
-    link: "./ui.apple.com",
+    img: "p4.svg",
+    iconLists: ["next.svg", "tail.svg", "ts.svg", "three.svg", "gsap.svg"],
+    link: "ui.apple.com",
   },
 ];
 
@@ -131,31 +131,31 @@ export const companies = [
     id: 1,
     name: "cloudinary",
     img: "jatin.png",
-    nameImg: "./cloudName.svg",
+    nameImg: "cloudName.svg",
   },
   {
     id: 2,
     name: "appwrite",
     img: "./app.svg",
-    nameImg: "./appName.svg",
+    nameImg: "appName.svg",
   },
   {
     id: 3,
     name: "HOSTINGER",
     img: "./host.svg",
-    nameImg: "./hostName.svg",
+    nameImg: "hostName.svg",
   },
   {
     id: 4,
     name: "stream",
     img: "./s.svg",
-    nameImg: "./streamName.svg",
+    nameImg: "streamName.svg",
   },
   {
     id: 5,
     name: "docker.",
     img: "./dock.svg",
-    nameImg: "./dockerName.svg",
+    nameImg: "dockerName.svg",
   },
 ];
 
@@ -165,42 +165,42 @@ export const workExperience = [
     title: "Frontend Engineer Intern",
     desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
     className: "md:col-span-2",
-    thumbnail: "./exp1.svg",
+    thumbnail: "exp1.svg",
   },
   {
     id: 2,
     title: "Mobile App Dev - JSM Tech",
     desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
     className: "md:col-span-2", // change to md:col-span-2
-    thumbnail: "./exp2.svg",
+    thumbnail: "exp2.svg",
   },
   {
     id: 3,
     title: "Freelance App Dev Project",
     desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
     className: "md:col-span-2", // change to md:col-span-2
-    thumbnail: "./exp3.svg",
+    thumbnail: "exp3.svg",
   },
   {
     id: 4,
     title: "Lead Frontend Developer",
     desc: "Developed and maintained user-facing features using modern frontend technologies.",
     className: "md:col-span-2",
-    thumbnail: "./exp4.svg",
+    thumbnail: "exp4.svg",
   },
 ];
 
 export const socialMedia = [
   {
     id: 1,
-    img: "./git.svg",
+    img: "git.svg",
   },
   {
     id: 2,
-    img: "./twit.svg",
+    img: "twit.svg",
   },
   {
     id: 3,
-    img: "./link.svg",
+    img: "link.svg",
   },
 ];
